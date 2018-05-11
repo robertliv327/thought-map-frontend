15253e3673e4fb7c122dad2c0f9dbaee450c0fd8
Robert Livaudais <robert.h.livaudais.19@dartmouth.edu>
2018-05-11 16:16:25 -0400

    username -> email

--- src/containers/sign-in.js
+++ src/containers/sign-in.js
@@ -8,22 +8,22 @@ class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      email: '',
       password: '',
     };
-    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
     this.onPasswordChange = this.onPasswordChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
-  onUsernameChange(event) {
-    this.setState({ username: event.target.value });
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
   }
   onPasswordChange(event) {
     this.setState({ password: event.target.value });
   }
   onSubmit(event) {
     const user = {
-      email: this.state.username,
+      email: this.state.email,
       password: this.state.password,
     };
     this.props.signinUser(user, this.props.history);
@@ -32,7 +32,7 @@ class SignIn extends Component {
     return (
       <div id="input-area">
         <h3>Please Sign In:</h3>
-        <input className="input" placeholder="Username" onChange={this.onUsernameChange} value={this.state.username} />
+        <input className="input" placeholder="Email" onChange={this.onEmailChange} value={this.state.email} />
         <input className="input" placeholder="Password" onChange={this.onPasswordChange} value={this.state.password} />
         <button className="button small" type="button" name="button" onClick={this.onSubmit}>Sign In</button>
       </div>
